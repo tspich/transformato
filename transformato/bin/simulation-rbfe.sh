567bfbca4952f64cce59974177dc4bceb11e0f6b
tspich <tspicher@tbi.univie.ac.at>
2026-06-10 17:11:38 +0200

    update simulation bash files

--- transformato/bin/simulation-rbfe.sh
+++ transformato/bin/simulation-rbfe.sh
@@ -1,18 +1,12 @@
 
-
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate fep
-
 path=$1
 
 cd ${path}
 pwd
 hostname
 
-
 istep=lig_in_complex
-python openmm_run.py -env complex -odcd ${istep}.dcd &> complex_out.log 
-
+python openmm_run.py -env complex -odcd ${istep}.dcd &> complex_out.log
 
 istep=lig_in_waterbox
-python openmm_run.py -env waterbox -odcd ${istep}.dcd  &> waterbox_out.log 
+python openmm_run.py -env waterbox -odcd ${istep}.dcd  &> waterbox_out.log
--- transformato/bin/simulation-rsfe.sh
+++ transformato/bin/simulation-rsfe.sh
@@ -1,18 +1,12 @@
 
-
-
-
 path=$1
 
 cd ${path}
 pwd
 hostname
 
-
 istep=lig_in_vacuum
 python openmm_run.py -env vacuum -odcd ${istep}.dcd &> vacuum_out.log
-
-
 
 istep=lig_in_waterbox
 python openmm_run.py -env waterbox -odcd ${istep}.dcd &> waterbox_out.log
